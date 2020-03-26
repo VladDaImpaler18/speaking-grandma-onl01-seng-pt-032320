e2e4db7e6e97332c3c8cf83ce0d65e644a77cfa8
Vladimir J <vladdaimpaler@protonmail.com>
2020-03-26 17:44:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,6 +17,5 @@ def speak_to_grandma(phrase)
      puts "I LOVE YOU TOO PUMPKIN!"
     elsif(phrase != phrase.upcase) #Shouting but not love
      puts "NO, NOT SINCE 1938!"
-  else (phrase != phrase.upcase)
-  "HUH?! SPEAK UP, SONNY"
+  else puts "HUH?! SPEAK UP, SONNY"
 end
