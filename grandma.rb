3cad7d546743f94659fd2ed2a8d3100e3e28e12d
Vladimir J <vladdaimpaler@protonmail.com>
2020-03-26 17:39:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,5 +16,6 @@ def speak_to_grandma(phrase)
     if(phrase == "I Love you grandma!".upcase)
       "I LOVE YOU TOO PUMPKIN!"
     else "NO, NOT SINCE 1938!"
-  else "HUH?! SPEAK UP, SONNY"
+  elsif (phrase != phrase.upcase)
+  "HUH?! SPEAK UP, SONNY"
   
